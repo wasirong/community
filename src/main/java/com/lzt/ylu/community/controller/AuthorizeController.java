5071fd401410e866f3be279a80465a1923b37747
Murasame <464742435@qq.com>
2020-09-16 00:36:26 +0800

    cookie and session

--- src/main/java/com/lzt/ylu/community/controller/AuthorizeController.java
+++ src/main/java/com/lzt/ylu/community/controller/AuthorizeController.java
@@ -11,7 +11,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.UUID;
 
 @Controller
@@ -33,7 +35,8 @@ public class AuthorizeController {
     @GetMapping("/callback")
     public String CallBack(@RequestParam(name = "code") String code,
                            @RequestParam(name = "state") String state,
-                           HttpServletRequest request) {
+                           HttpServletRequest request,
+                           HttpServletResponse response) {
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setCode(code);
         accessTokenDTO.setState(state);
@@ -53,13 +56,15 @@ public class AuthorizeController {
             System.out.println("存入database");
             User user = new User();
             user.setName(gitUser.getName());
-            user.setToken(UUID.randomUUID().toString());
+            String token = UUID.randomUUID().toString();
+            user.setToken(token);
             user.setAccountId(String.valueOf(gitUser.getId()));
             user.setGmtCreate(System.currentTimeMillis());
             user.setGmtModify(user.getGmtCreate());
             userMapper.insert(user);
+            response.addCookie(new Cookie("token",token));
         }
-        request.getSession().setAttribute("user", gitUser);
+//        request.getSession().setAttribute("user", gitUser);
         // Login success
         return "redirect:/";
     }
--- src/main/java/com/lzt/ylu/community/mapper/UserMapper.java
+++ src/main/java/com/lzt/ylu/community/mapper/UserMapper.java
@@ -3,9 +3,14 @@ package com.lzt.ylu.community.mapper;
 import com.lzt.ylu.model.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface UserMapper {
     @Insert("insert into User (name, account_id, token, gmt_create, gmt_modify) values (#{name}, #{accountId}, #{token}, #{gmtCreate}, #{gmtModify})")
     void insert(User user);
+
+    @Select("select * from User where token = #{token}")
+    User findByToken(@Param("token") String token);
 }
